8dae2a233a4b46094ea4f9bd717e1ba6c2a98e43
darrenlo-lab <darren.jw.pro@gmail.com>
2019-12-09 13:36:10 +0800

    CarLoan

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val calculationButton: Button = findViewById(R.id.buttonCalculate)
+        calculationButton.setOnClickListener{calculate()}
+        val resetButton: Button = findViewById(R.id.buttonReset)
+        resetButton.setOnClickListener{reset()}
+
+    }
+    private fun reset(){
+        editTextCarPrice.setText("")
+        editTextDownPayment.setText("")
+        editTextInterestRate.setText("")
+        editTextLoanPeriod.setText("")
+        AnswerLoan.setText("")
+        interest1.setText("")
+        Repayment.setText("")
+    }
+    private fun calculate(){
+
+        val carPrice = editTextCarPrice.text.toString().toDouble()
+        val downPayemnt = editTextDownPayment.text.toString().toDouble()
+        val interestRate = editTextInterestRate.text.toString().toDouble()
+        val period = editTextLoanPeriod.text.toString().toDouble()
+        val totalLoan = carPrice-downPayemnt
+        val totalInterest = totalLoan*interestRate*period
+        val totalRepayment = (totalLoan + totalInterest)/period/12
+
+        AnswerLoan.text = String.format("%.2f", totalLoan)
+        interest1.text = String.format("%.2f", totalInterest)
+        Repayment.text =  String.format("%.2f", totalRepayment)
+    }
+}
